24a5a631617b0cefc7af69c93b075682cbe5b315
Jay Conrod <jayconrod@gmail.com>
2018-04-22 20:35:25 -0400

    Initial commit: license, build, ci, formatting

---
+++ README.md
@@ -0,0 +1,5 @@
+CodeSwitch is a fast, lightweight virtual machine with support for
+multiple languages.
+
+CodeSwitch is in a very early stage of development, so there's no
+releases or documentation yet. Check back later!
---
+++ codeswitch.h
@@ -0,0 +1,11 @@
+// Copyright Jay Conrod. All rights reserved.
+
+// This file is part of CodeSwitch. Use of this source code is governed by
+// the 3-clause BSD license that can be found in the LICENSE.txt file.
+
+#ifndef codeswitch_h
+#define codeswitch_h
+
+namespace codeswitch {}
+
+#endif
