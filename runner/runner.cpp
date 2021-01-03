acc5a3773e6e0a7c7153baea5a53cf1a3cda3d52
Jay Conrod <jayconrod@gmail.com>
2021-01-03 11:25:51 -0500

    Add Runner

---
+++ runner/runner.cpp
@@ -0,0 +1,27 @@
+// Copyright Jay Conrod. All rights reserved.
+
+// This file is part of CodeSwitch. Use of this source code is governed by
+// the 3-clause BSD license that can be found in the LICENSE.txt file.
+
+#include "runner.h"
+
+#include <functional>
+#include <thread>
+
+namespace codeswitch {
+namespace internal {
+
+void Runner::run(std::function<void()>&& task) {
+  // TODO: implement M:N threading.
+  //
+  // We can get the number of machine threads with
+  // std::thread::hardware_concurrency.
+  //
+  // However, we need a way for a thread to release its resources (allowing
+  // other threads to spawn) when it's blocked, for example, on a lock or
+  // on I/O.
+  new std::thread(task);
+}
+
+}  // namespace internal
+}  // namespace codeswitch
---
+++ runner/runner.h
@@ -0,0 +1,38 @@
+// Copyright Jay Conrod. All rights reserved.
+
+// This file is part of CodeSwitch. Use of this source code is governed by
+// the 3-clause BSD license that can be found in the LICENSE.txt file.
+
+#ifndef runner_h
+#define runner_h
+
+#include <deque>
+#include <functional>
+#include <mutex>
+#include <thread>
+#include <vector>
+#include "common/common.h"
+
+namespace codeswitch {
+namespace internal {
+
+/**
+ * Runner executes tasks asynchronously on a set of background threads.
+ * 
+ * Currently, this is a stub implementation that spawns a new thread for each
+ * task. Later, Runner will implement M:N threading.
+ */
+class Runner {
+ public:
+  NON_COPYABLE(Runner)
+  Runner();
+
+  void run(std::function<void()>&& task);
+};
+
+extern Runner runner;
+
+}  // namespace internal
+}  // namespace codeswitch
+
+#endif
